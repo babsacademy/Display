f0e894b27e04cffa4fc9fd85d7f94a07f1fbcf1d
babsacademy <33089265+babsacademy@users.noreply.github.com>
2023-04-24 10:49:50 +0000

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import sqlite3 from 'sqlite3';
+
+const db = new sqlite3('articles.db');
+
+const Home = () => {
+  const articles = db.query('SELECT * FROM articles');
+
+  // Write the articles to a file
+  const articlesFile = fs.createWriteStream('articles.json');
+  articlesFile.write(JSON.stringify(articles, null, 2));
+  articlesFile.close();
+
+  return (
+    <div>
+      <h1>Articles</h1>
+      <ul>
+        {articles.map((article) => (
+          <li key={article.id}>
+            <h2>{article.title}</h2>
+            <p>{article.content}</p>
+            <img src={article.photo} alt={article.title} />
+            <p>{article.date}</p>
+            <a href={article.link}>{article.site}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
